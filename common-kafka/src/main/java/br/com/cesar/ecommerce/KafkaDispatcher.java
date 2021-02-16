2ba1098cdcc9a4ecdf6c152292c9603ff2549414
Cesar Alcancio <cesar.alcancio@payclip.com>
2021-02-16 16:33:21 -0600

    Add field in servlet to keep the connection with Kafka even when the server is off (cluster configured)

--- common-kafka/src/main/java/br/com/cesar/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/br/com/cesar/ecommerce/KafkaDispatcher.java
@@ -34,7 +34,7 @@ class KafkaDispatcher<T> implements Closeable {
 
     private Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         return properties;
--- service-http/src/main/java/br/com/cesar/ecommerce/NewOrderServlet.java
+++ service-http/src/main/java/br/com/cesar/ecommerce/NewOrderServlet.java
@@ -11,40 +11,45 @@ import java.util.concurrent.ExecutionException;
 
 public class NewOrderServlet extends HttpServlet {
 
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<Order>();
+    private final KafkaDispatcher<Email> emailDispatcher = new KafkaDispatcher<Email>();
+
+    @Override
+    public void destroy() {
+        this.orderDispatcher.close();
+        this.emailDispatcher.close();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var topicNewOrder = "ECOMMERCE_NEW_ORDER";
         var topicSendEmail = "ECOMMERCE_SEND_EMAIL";
 
-        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
-                var orderId = UUID.randomUUID().toString();
-                var reqEmail = String.valueOf(req.getParameter("email"));
-                var reqAmount = String.valueOf(req.getParameter("amount"));
+        var orderId = UUID.randomUUID().toString();
+        var reqEmail = String.valueOf(req.getParameter("email"));
+        var reqAmount = String.valueOf(req.getParameter("amount"));
 
-                var randomEmail = isNullOrEmpty(reqEmail) ?
-                        Math.random() + "@cesar.com" : reqEmail;
-                var amount = isNullOrEmpty(reqAmount) ?
-                        new BigDecimal(Math.random() * 5000 + 1) : new BigDecimal(reqAmount);
+        var randomEmail = isNullOrEmpty(reqEmail) ?
+                Math.random() + "@cesar.com" : reqEmail;
+        var amount = isNullOrEmpty(reqAmount) ?
+                new BigDecimal(Math.random() * 5000 + 1) : new BigDecimal(reqAmount);
 
-                var emailValue = "Thank you. We are processing your order";
-                var email = new Email(emailValue, emailValue);
+        var emailValue = "Thank you. We are processing your order";
+        var email = new Email(emailValue, emailValue);
 
-                Order order = new Order("1", orderId, randomEmail, amount);
+        Order order = new Order("1", orderId, randomEmail, amount);
 
-                try {
-                    orderDispatcher.send(topicNewOrder, randomEmail, order);
-                    emailDispatcher.send(topicSendEmail, randomEmail, email);
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                resp.setStatus(HttpServletResponse.SC_OK);
-                resp.getWriter().println("New order sent: " + order.toString());
-            }
+        try {
+            orderDispatcher.send(topicNewOrder, randomEmail, order);
+            emailDispatcher.send(topicSendEmail, randomEmail, email);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("New order sent: " + order.toString());
     }
 
     private boolean isNullOrEmpty(String s) {
